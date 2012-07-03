42d051524f35d32749cd3b42b900bfc9378c318e
Richard Cunningham <github@richardcunningham.co.uk>
2012-07-03 08:08:16 -0400

    Whitespace

--- array_subset.php
+++ array_subset.php
@@ -5,7 +5,7 @@
  *  
  *  Example:  
  *
- *  print_r(array_subset(array("x","y"),array("x" => 1, "y" => "2", "z" => 3)));
+ *  print_r(array_subset(array("x", "y"), array("x" => 1, "y" => 2, "z" => 3)));
  *	Array
  *	(
  *		 [x] => 1
@@ -20,7 +20,7 @@ function array_subset($whitelist, $array)
     {
         foreach($array as $key => $value)
         {
-            if(in_array($key,$whitelist,TRUE))
+            if(in_array($key, $whitelist, TRUE))
                 $out_array[$key] = $value;
         }
     }
